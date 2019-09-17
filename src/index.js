5247925df7ac607fde8b28b4dcff01c528ed56bc
brenodev <breno.souza.developer@gmail.com>
2019-09-16 22:56:56 -0300

    adicionado express e body-parser e criado index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// definição do express
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+//criando a aplicação simplremente chamando a função express
+const app = express();
+
+//indicando que o body-parser será usando
+
+app.use(bodyParser.json()); // para entender as informações enviadas para API em json
+app.use(bodyParser.urlencoded({ extended: false })); // para entender quando for passado parametros via url
+
+// ROTAS
+
+app.listen(3000); // definindo a porta da aplicação
